a2b56d54bba89ee0f320b159218e46583e2ce8e0
rodolphostoppan <rodolpho.toppan@db1.com.br>
2024-01-11 14:40:38 -0300

    feature: refactor project view

--- src/modules/project/ProjectView.vue
+++ src/modules/project/ProjectView.vue
@@ -3,11 +3,11 @@
     <h1>Projeto</h1>
     <p>Por fim, só preciso de mais alguns dados pra mostrar o projeto do seu cliente:</p>
     <section>
-      <state-select name="Estado" />
-      <city-select name="Cidade" />
-      <address-input name="Endereço" />
-      <amount-input name="Potência dos módulos (W)" type="number" />
-      <submit-bill name="DIMENSIONAR" route="sizing" />
+      <state-select name="Estado" :options="states" v-model="project.location.state" />
+      <city-select name="Cidade" :options="cities" v-model="project.location.city" />
+      <address-input name="Endereço" v-model="project.location.address" />
+      <amount-input name="Potência dos módulos (W)" type="number" v-model="project.modulesPower" />
+      <submit-bill name="DIMENSIONAR" @click="projectSizing" />
     </section>
   </main>
 </template>
@@ -16,6 +16,10 @@
 import InputTextComponent from '@/components/input-text-component.vue'
 import InputSelectComponent from '@/components/input-select-component.vue'
 import Button from '@/components/button-component.vue'
+import { ProjectService } from './services/project.service'
+import { HTTP_CLIENT, type HttpClient } from '@/infra/http/http'
+import { inject } from 'vue'
+import { Project } from './entities/project'
 
 export default {
   components: {
@@ -27,17 +31,36 @@ export default {
   },
   data() {
     return {
-      showModal: false
+      project: new Project(),
+      projectService: new ProjectService(inject(HTTP_CLIENT) as HttpClient),
+      states: [
+        { id: 1, item: 'pr' },
+        { id: 3, item: 'SP' },
+        { id: 4, item: 'RS' },
+        { id: 5, item: 'SC' },
+        { id: 6, item: 'MG' },
+        { id: 7, item: 'RJ' },
+        { id: 8, item: 'ES' },
+        { id: 9, item: 'MT' }
+      ],
+      cities: [
+        { id: 1, item: 'Maringá' },
+        { id: 3, item: 'Londrina' },
+        { id: 4, item: 'Curitiba' },
+        { id: 5, item: 'Marialva' },
+        { id: 6, item: 'Sarandi' },
+        { id: 7, item: 'Colorado' },
+        { id: 8, item: 'Santa Fé' },
+        { id: 9, item: 'Paiçandu' }
+      ]
     }
   },
   methods: {
-    openModal() {
-      this.showModal = true
-    },
-    closeModal() {
-      this.showModal = false
-    },
-    addBill() {}
+    async projectSizing() {
+      await this.projectService.save(this.project).then(() => {
+        this.$router.push({ name: 'sizing' })
+      })
+    }
   }
 }
 </script>
