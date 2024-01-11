47cf95eb8c2e811c6c7ca5829693f36d009b6497
rodolphostoppan <rodolpho.toppan@db1.com.br>
2024-01-11 08:47:41 -0300

    feature: refactor project view

---
+++ src/modules/project/services/project.service.ts
@@ -0,0 +1,31 @@
+import type { HttpClient } from '@/infra/http/http'
+import { Project } from '../entities/project'
+
+export class ProjectService {
+  constructor(private readonly httpClient: HttpClient) {}
+  save(project: Project) {
+    const payload = project.format()
+    return this.httpClient.post('/projects', payload)
+  }
+
+  update(project: Project) {
+    const payload = project.format()
+    return this.httpClient.put(`/projects/${project.id}`, payload)
+  }
+
+  delete(id: string) {
+    return this.httpClient.delete(`/projects/${id}`, null)
+  }
+
+  getProjectById(id: string): Promise<Project> {
+    return this.httpClient.get(`/projects/${id}`).then(({ data }) => {
+      return new Project(data)
+    })
+  }
+
+  getAllProjects(): Promise<Project[]> {
+    return this.httpClient.get('/projects').then(({ data }) => {
+      return data.map((project: any) => new Project(project))
+    })
+  }
+}
